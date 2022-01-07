4e96244d86cd403bd911720b4c412f8f6a2f1ef2
trevordcampbell <55037769+trevordcampbell@users.noreply.github.com>
2022-01-07 22:44:11 +0000

    fix: updated tailwindcss to v3.0+ and updated tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
